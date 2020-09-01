14788ea2bf62792516930302126d5f778044770c
Benjamin Liu <bennliu01@gmail.com>
2020-09-01 00:51:43 -0700

    Routing interplate, grid setup, topbar components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter, Route, Switch, Redirect
+} from 'react-router-dom';
+import {
+  Grid, Typography, Paper
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+//Import necessary components
+import Topbar from './Components/Topbar/Topbar';
+
+//Custom Material UI Styles
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    paddingLeft: 100,
+    paddingRight: 100,
+    paddingTop: 50,
+    paddingBottom: 100,
+    height: '100vh'
+  },
+  paperTop: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  paperBottom: {
+    minHeight: 700
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <HashRouter>
+    <div className={classes.root}>
+      <Grid container spacing={3} alignItems="stretch">
+        <Grid item xs={12}>
+          <Paper className={classes.paperTop}>
+            <div className="logoDiv"/>
+            <Topbar/>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paperTop, classes.paperBottom}>
+            <Switch>
+              {
+                <Route exact path="/"
+                     render={() =>
+                      <div> Body </div>}
+                   />
+              }
+              {
+                <Route exact path="/about"
+                     render={() =>
+                      <div> About </div>}
+                   />
+              }
+              {
+                <Route exact path="/resources"
+                     render={() =>
+                      <div> resources </div>}
+                   />
+              }
+              {
+                <Route exact path="/schedule"
+                     render={() =>
+                      <div> schedule </div>}
+                   />
+              }
+              {
+                <Route exact path="/contact"
+                     render={() =>
+                      <div> contact </div>}
+                   />
+              }
+            </Switch>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Components/Topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import { blue, cyan } from "@material-ui/core/colors";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  buttonElem: {
+    flexGrow: 1,
+    width: '20%',
+    color: 'cyan'
+  }
+}));
+
+function Topbar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+            <Button className={classes.buttonElem} component={ Link } to= "/about">About</Button>
+            <Button className={classes.buttonElem} component={ Link } to= "/resources">Resources</Button>
+            <Button className={classes.buttonElem} component={ Link } to= "/schedule">Schedule</Button>
+            <Button className={classes.buttonElem} component={ Link } to= "/contact">Contact Us</Button>
+            </Toolbar>
+        </AppBar>
+        </div>
+    );
+}
+
+export default Topbar
