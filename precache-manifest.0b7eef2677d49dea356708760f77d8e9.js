11d5e49586d7909adb8e96fe0526d8a76d7a81d0
Benjamin Liu <bennliu01@gmail.com>
2020-09-01 17:03:52 -0700

    Updates

---
+++ precache-manifest.0b7eef2677d49dea356708760f77d8e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e6ad3686137b8843a4150bcdb1dff3",
+    "url": "/BlueprintReactApp/index.html"
+  },
+  {
+    "revision": "4b86904dc1e81d79ada8",
+    "url": "/BlueprintReactApp/static/css/main.f9bca317.chunk.css"
+  },
+  {
+    "revision": "6e5393a0c1b3b2151391",
+    "url": "/BlueprintReactApp/static/js/2.c819cf08.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BlueprintReactApp/static/js/2.c819cf08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b86904dc1e81d79ada8",
+    "url": "/BlueprintReactApp/static/js/main.cc4e8f73.chunk.js"
+  },
+  {
+    "revision": "e3eff2f9542d1a92d89d",
+    "url": "/BlueprintReactApp/static/js/runtime-main.b64047b7.js"
+  },
+  {
+    "revision": "2ae6293bc3710f7f63afac7bd3fedfbd",
+    "url": "/BlueprintReactApp/static/media/starImage.2ae6293b.jpeg"
+  }
+]);
